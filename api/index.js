a13d9ae1fcf3d80b60f054bb1cc32c3eacfe3442
Charles Buckley <charlesbuckley122@gmail.com>
2023-01-17 10:58:57 -0800

    adds api folder

---
+++ api/index.js
@@ -0,0 +1,18 @@
+//express sever
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// enable body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, () => console.log(`Server started on post ${port}`));
